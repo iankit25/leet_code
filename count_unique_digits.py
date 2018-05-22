f434ba3079246bb1b4806cda8db68cae9e2623c9
ishita <ishita13316@gmail.com>
2018-05-22 16:14:07 -0400

    count_unique

--- count_unique_digits.py
+++ count_unique_digits.py
@@ -1,15 +1,30 @@
-#Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+#Given a non-negative integer n, count all numbers with unique digits, x,
+
 class Solution(object):
-	def count(self,num_list):
-		for ele in num_list:
+	def __init__(self):
+		self.unique_count = {}
+
+	def count(self,n,num,num_list):
+		if n == 0 :
+			return 
+		j = 0
+		for i in num_list:
+			j+=1
+			key =num+str(i)
+			self.unique_count[key] = 1
+			if key[0]!='0':
+				self.count(n-1,num+str(i),num_list[:j]+num_list[j+1:])
 			
 
 
-
-
-    def countNumbersWithUniqueDigits(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        
+	def countNumbersWithUniqueDigits(self, n):
+		"""
+		:type n: int
+		:rtype: int
+		"""
+		self.count(n,'',[0,1,2,3,4,5,6,7,8,9])
+		print self.unique_count.keys()
+		return len(self.unique_count.keys())
+		
+obj = Solution()
+print obj.countNumbersWithUniqueDigits(2)
